f9e72b63b4f67c855d05fc85345ce1a898e0dbbb
Dovo23 <paulmaerz007@gmail.com>
2026-06-10 09:56:51 +0200

    Initialize Maven Spring Boot project

---
+++ src/main/java/de/restvsgraphql/warframe/RestVsGraphqlWarframeApplication.java
@@ -0,0 +1,12 @@
+package de.restvsgraphql.warframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVsGraphqlWarframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestVsGraphqlWarframeApplication.class, args);
+    }
+}
